bf15fc334fdb73876f218b100c9ccb7e308c591d
jnr <13692453351@163.com>
2020-10-04 19:56:23 +0800

    添加了 test 组件@JNR

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,34 +1,34 @@
 <template>
   <div>
-    <b-container style="margin:100px;padding:20px;">
+    <b-container style="margin:50px;padding:10px;">
       <b-row>
         <div class="border border-primary"
-             style="margin:20px;padding:20px;">
+             style="margin:10px;padding:10px;">
           <img src="@/assets/img/logo.png"
                alt="">
         </div>
       </b-row>
       <b-row>
         <div class="border border-primary"
-             style="margin:20px;padding:20px;">
+             style="margin:10px;padding:10px;">
           <router-link to="/">index</router-link>
         </div>
       </b-row>
       <b-row>
         <div class="border border-info"
-             style="margin:20px;padding:20px;">
+             style="margin:10px;padding:10px;">
           <router-link to="/home">home</router-link>
         </div>
       </b-row>
       <b-row>
         <div class="border border-success"
-             style="margin:20px;padding:20px;">
+             style="margin:10px;padding:10px;">
           <router-link to="/music">music</router-link>
         </div>
       </b-row>
       <b-row>
         <div class="border border-success"
-             style="margin:20px;padding:20px;">
+             style="margin:10px;padding:10px;">
           <router-link to="/test">test</router-link>
         </div>
       </b-row>
